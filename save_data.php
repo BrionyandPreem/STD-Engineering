bd5eccf4182ac68fbad94a8a11f223f438cf4b80
BrionyandPreem <brionyrakpreem999@gmail.com>
2026-03-06 20:31:15 +0700

    Add files via upload

--- save_data.php
+++ save_data.php
@@ -1,42 +1,45 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-if (SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'] ?? '';
+    $company = $_POST['company'] ?? '';
+    $phone = $_POST['phone'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $service = $_POST['service'] ?? '';
+    $province = $_POST['province'] ?? '';
+    $industrial_estate = $_POST['industrial_estate'] ?? '';
+    $more_details = $_POST['more_details'] ?? '';
 
-    name = POST['name'] '';
-    company = POST['company'] '';
-    phone = POST['phone']  '';
-    email = POST['email'] '';
-    service = POST['service'] '';
-    province = POST['province'] '';
-    industrial_estate = POST['industrial_estate']  '';
-    more_details = POST['more_details'] '';
+    $to = "Info@stdengineering-th.com";
+    $subject = "=?UTF-8?B?".base64_encode("มีใบเสนอราคาใหม่จากเว็บไซต์ STD Engineering")."?=";
 
-    to = "Info@stdengineering-th.com";
-    subject = "มีใบเสนอราคาใหม่จากเว็บไซต์";
+    $message = "
+    มีการส่งแบบฟอร์มใหม่เข้ามาจากเว็บไซต์
 
-    message = "
-    มีการส่งแบบฟอร์มใหม่เข้ามา
+    ชื่อ: $name
+    บริษัท: $company
+    เบอร์โทร: $phone
+    อีเมล: $email
 
-    ชื่อ: name
-    บริษัท: company
-    เบอร์โทร: phone
-    อีเมล: email
-
-    ประเภทบริการ: service
-    จังหวัด: province
-    นิคมอุตสาหกรรม: industrial_estate
+    ประเภทบริการ: $service
+    จังหวัด: $province
+    นิคมอุตสาหกรรม: $industrial_estate
 
     รายละเอียดเพิ่มเติม:
-    more_details
+    $more_details
     ";
 
-    headers = "From: noreply@yourdomain.com\r\n";
-    headers .= "Reply-To: email\r\n";
+    $headers = "From: Info@stdengineering-th.com\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-    if(mail(to, subject, message, headers)){
-        echo "<script>alert('ส่งข้อมูลเรียบร้อยแล้ว'); window.location.href='index.html';</script>";
+    if(mail($to, $subject, $message, $headers)){
+        echo "<script>
+                alert('ส่งข้อมูลเรียบร้อยแล้ว'); 
+                window.location.href='index.html';
+              </script>";
     } else {
-        echo "เกิดข้อผิดพลาดในการส่งอีเมล";
+        echo "เกิดข้อผิดพลาดในการส่งอีเมล กรุณาลองใหม่อีกครั้ง";
     }
 }
 ?>
