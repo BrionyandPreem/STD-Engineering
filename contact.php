f5b405de2392976109b60a1017dea0955db8959b
BrionyandPreem <brionyrakpreem999@gmail.com>
2026-03-06 20:28:10 +0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,38 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	
+    $name = $_POST['name'] ?? '';
+    $company = $_POST['company'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $service = $_POST['service'] ?? '';
+    $message_detail = $_POST['message'] ?? '';
+
+    $to = "Info@stdengineering-th.com";
+    $subject = "=?UTF-8?B?".base64_encode("มีการติดต่อใหม่จากเว็บไซต์ STD Engineering")."?=";
+
+    $email_content = "มีการส่งแบบฟอร์มใหม่เข้ามาจากหน้าเว็บไซต์\n\n";
+    $email_content .= "ชื่อ-นามสกุล: $name\n";
+    $email_content .= "บริษัท: $company\n";
+    $email_content .= "อีเมลติดต่อ: $email\n";
+    $email_content .= "ประเภทบริการที่สนใจ: $service\n\n"; 
+    $email_content .= "รายละเอียดเพิ่มเติม:\n$message\n";
+
+    $headers = "From: Info@stdengineering-th.com\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    if(mail($to, $subject, $email_content, $headers)){
+        echo "<script>
+                alert('ส่งข้อมูลเรียบร้อยแล้ว ขอบคุณที่ติดต่อเราค่ะ');
+                window.location.href='index.html'; 
+              </script>";
+    } else {
+        echo "<script>
+                alert('เกิดข้อผิดพลาดในการส่งอีเมล กรุณาลองใหม่อีกครั้ง');
+                window.history.back();
+              </script>";
+    }
+}
+
+?>
